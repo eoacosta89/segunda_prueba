f2b096242684950bf30fe321a6860bab78e1b25c
Emilson Acosta <emilson@ubuntu.(none)>
2013-02-20 05:09:29 -0600

    Incio del proyecto

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author emilson
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Proyecto de prueba");
+		System.out.println("Codigo inicial a subir a git");
+	}
+
+}
